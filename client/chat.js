2907890185d8629389ad5d9a95c189d836a38b1b
john <john@MacBook-Air-john.local>
2018-11-09 17:00:56 +0300

    added support for long messages

--- client/chat.js
+++ client/chat.js
@@ -9,16 +9,37 @@ function Chat(player_name, max_count, sender) {
     this.sender = sender;
     
     this.addMessage = function(data) {
+        var splitMessages = this.formatMsg(data.msg);
+        var d;
+        for (var i = 0; i < splitMessages.length; i++) {
+            d = data;
+            d.msg = splitMessages[i];
+            if (i != 0) {
+                d.continued = true;
+            }
+            this.addSingleMessage(d);
+        }
+    }
+    
+    this.addSingleMessage = function(data) {
         var msg = document.createElement("div");
         if (data.user == player_name) {
             msg.classList.add("my-msg");
-            msg.innerHTML = `<p class="name">${data.user}</p>: <p>${data.msg}</p>`;
+            if (data.continued) {
+                msg.innerHTML = `<p>${data.msg}</p>`;    
+            } else {
+                msg.innerHTML = `<p class="name">${data.user}</p>: <p>${data.msg}</p>`;
+            }
         } else if (data.user == "__system__") {
             msg.classList.add("system-msg");
             msg.innerHTML = `${data.msg}`;
         } else {
             msg.classList.add("other-msg");
-            msg.innerHTML = `<p class="name">${data.user}</p>: <p>${data.msg}</p>`;
+            if (data.continued) {
+                msg.innerHTML = `<p>${data.msg}</p>`;    
+            } else {
+                msg.innerHTML = `<p class="name">${data.user}</p>: <p>${data.msg}</p>`;
+            }
         }
         msg.classList.add("chat-msg");
         
@@ -30,7 +51,15 @@ function Chat(player_name, max_count, sender) {
             this.message_count++;
         }
     };
-
+    
+    this.formatMsg = function(msg){
+        //25 is a length of a single message
+        
+        var a = [];
+        a = msg.match(/.{1,25}/g); 
+        return a;
+    }
+    
     this.sendMessage = function() {
         if (chat_input.value == "") {
             return;
@@ -39,5 +68,5 @@ function Chat(player_name, max_count, sender) {
         this.addMessage({user: this.username, msg: chat_input.value});
         chat_input.value = "";
     }
-
+    
 }
